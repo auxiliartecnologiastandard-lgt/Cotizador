78fe041397f10c5e528570ab57b6f71f18f4e742
auxiliartecnologiastandard-lgt <auxiliartecnologiastandard@gmail.com>
2026-01-06 08:45:45 -0500

    Add files via upload

--- Cotizador_Coandes-copia/Computadores/app.py
+++ Cotizador_Coandes-copia/Computadores/app.py
@@ -121,7 +121,7 @@ if st.button("🚀 CALCULAR VALOR"):
     
     # 3. Filtros de Realidad (Anclas de precio)
     if valor_procesador <= 5: 
-        precio_base = np.clip(precio_base * 0.30, 90000, 150000)
+        precio_base = np.clip(precio_base * 0.25, 100000, 150000)
     elif valor_procesador <= 15: 
         precio_base = np.clip(precio_base * 0.40, 100000, 150000)
     elif valor_procesador <= 30:
