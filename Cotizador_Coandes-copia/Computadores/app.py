b013948173625b097fe735c96ad45249c7d853de
auxiliartecnologiastandard-lgt <auxiliartecnologiastandard@gmail.com>
2026-01-08 09:04:57 -0500

    Add files via upload

--- Cotizador_Coandes-copia/Computadores/app.py
+++ Cotizador_Coandes-copia/Computadores/app.py
@@ -152,7 +152,7 @@ if st.button("🚀 CALCULAR VALOR"):
         precio_base = np.clip(precio_base * 0.25, 100000, 150000)
     # 3.1 Ancla para el los procesadores I3 
     elif valor_procesador <= 15:
-        if valor_ram == 7 and valor_disco_ia > 480:
+        if valor_ram == 7 or valor_disco_ia > 480:
             precio_base = np.clip(precio_base * 0.80, 200000, 600000)
         else:
             precio_base = np.clip(precio_base * 0.50, 120000, 300000)
