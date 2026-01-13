c31648d26a10282c3f20cffba87e4fe2d34ff8b1
auxiliartecnologiastandard-lgt <auxiliartecnologiastandard@gmail.com>
2026-01-13 08:43:35 -0500

    Add files via upload

--- Cotizador_Coandes-copia/Lobby.py
+++ Cotizador_Coandes-copia/Lobby.py
@@ -4,7 +4,7 @@ import os
 
 st.markdown("""
     <style>
-    .stApp { background-color: #F0F4F2; }
+    .stApp { background-color: #F5F5DC; }
     * { color: #000000 !important; }
     </style>
     """, unsafe_allow_html=True)
