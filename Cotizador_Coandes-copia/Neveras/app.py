e15512d31326fa637eb2d2b88f9693e5ed42c7c1
auxiliartecnologiastandard-lgt <auxiliartecnologiastandard@gmail.com>
2026-01-14 09:41:13 -0500

    Add files via upload

--- Cotizador_Coandes-copia/Neveras/app.py
+++ Cotizador_Coandes-copia/Neveras/app.py
@@ -3,15 +3,6 @@ import pandas as pd
 import joblib
 import numpy as np
 
-if "v_compra" not in st.session_state:
-    st.session_state.v_compra = ""
-
-if "v_venta" not in st.session_state:
-    st.session_state.v_venta = ""
-
-if "mostrar_venta" not in st.session_state:
-    st.session_state.mostrar_venta = False
-
 # ELIMINAR BARRA LATERAL
 st.set_page_config(
     page_title="Cotizador Coandes",
@@ -117,24 +108,20 @@ valor_Sistema_de_enfriamiento = SE_opciones[seleccion]
 
 st.divider()
 
+# --- CÁLCULO FINAL ---
+
 if st.button("🗿 CALCULAR VALOR"):
 
+    # 1. Predicción
     entrada = np.array([[valor_marca, valor_litro_final, valor_Sistema_de_enfriamiento]])
     precio_base = modelo.predict(entrada)[0]
 
+    # 2. Redondear precios
     precio_base_redondo = round(precio_base / 10000) * 10000
     precio_venta_redondo = round((precio_base_redondo * 1.4) / 10000) * 10000
 
-    st.session_state.v_compra = f"${precio_base_redondo:,.0f}".replace(",", ".")
-    st.session_state.v_venta = f"${precio_venta_redondo:,.0f}".replace(",", ".")
-    st.session_state.mostrar_venta = False
+    # 3. Resultados
+    v_venta = f"${precio_venta_redondo:,.0f}".replace(",", ".")
+    v_compra = f"${precio_base_redondo:,.0f}".replace(",", ".")
 
-    if st.session_state.v_compra:
-        st.info(f"### Oferta de Compraventa: {st.session_state.v_compra}")
-
-        if st.button("💰 Mostrar precio de venta"):
-            st.session_state.mostrar_venta = True
-
-    if st.session_state.mostrar_venta:
-        st.success(f"### Precio de Venta Sugerido: {st.session_state.v_venta}")
-
+    st.info(f"### Oferta de Compraventa: {v_compra}")
