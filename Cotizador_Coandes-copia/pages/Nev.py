b7cccb867819cf958897a38444bfdec9ed368ab3
auxiliartecnologiastandard-lgt <auxiliartecnologiastandard@gmail.com>
2026-01-14 15:07:29 -0500

    Add files via upload

--- Cotizador_Coandes-copia/pages/Nev.py
+++ Cotizador_Coandes-copia/pages/Nev.py
@@ -137,22 +137,22 @@ def main():
         #RESULTADO
         st.info(f"### Oferta de Compraventa: {st.session_state['v_compra']}")
 
-        if st.session_state.get("precio_calculado"):
-            if st.button("Crear contrato"):
-                if "v_compra" in st.session_state:
-                    st.session_state["datos_cotizador"] = {
-                        "Origen": "nevera",
-                        "Marca": st.session_state["valor_marca"],
-                        "Litros": st.session_state["valor_litro_final"],
-                        "Sistema": st.session_state["valor_Sistema_de_enfriamiento"],
-                        "Precio": st.session_state["v_compra"],
-                        "Tasa": st.session_state["valor_tasa"]
-                    }
-                    st.success("Contrato generado correctamente!")
+    if st.session_state.get("precio_calculado"):
+        if st.button("Crear contrato"):
+            if "v_compra" in st.session_state:
+                st.session_state["datos_cotizador"] = {
+                     "Origen": "nevera",
+                    "Marca": st.session_state["valor_marca"],
+                    "Litros": st.session_state["valor_litro_final"],
+                    "Sistema": st.session_state["valor_Sistema_de_enfriamiento"],
+                     "Precio": st.session_state["v_compra"],
+                    "Tasa": st.session_state["valor_tasa"]
+                   }
+                st.success("Contrato generado correctamente!")
 
                     # Esto funciona si el nombre de la página en Streamlit es 'Contrato_Standard'
-                    st.switch_page("pages/Contrato_Standard.py")  # NO usar "pages/Contrato_Standard.py"
+                st.switch_page("pages/Contrato_Standard.py")  # NO usar "pages/Contrato_Standard.py"
                     
-                else:
-                    st.warning("Primero calcula el precio en el cotizador")
+            else:
+                st.warning("Primero calcula el precio en el cotizador")
 main()
