5d3a2c9510fcc0cbe634d32ea92dc83fdfbad69e
auxiliartecnologiastandard-lgt <auxiliartecnologiastandard@gmail.com>
2026-01-14 07:45:13 -0500

    Add files via upload

--- Cotizador_Coandes-copia/Lobby.py
+++ Cotizador_Coandes-copia/Lobby.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import os
 
-st.toast('Bienvenido al Sistema Coandes', icon='🏢')    
+st.toast('Bienvenido al Sistema de Cotización Inteligente', icon='🏢')
+
 # 0. Ubicar la imagen del logo
 ruta_base = os.path.dirname(__file__)
 ruta_logo = os.path.join(ruta_base, "Standard_logo.png")
