55daa1a384c411fdaa02808cbc9534857e01dece
auxiliartecnologiastandard-lgt <auxiliartecnologiastandard@gmail.com>
2026-01-15 16:50:00 -0500

    Add files via upload

--- Cotizador_Coandes-copia/pages/Contrato_Standard.py
+++ Cotizador_Coandes-copia/pages/Contrato_Standard.py
@@ -192,10 +192,10 @@ elif datos["Origen"] == "Computador":
     qr.save("qr_temp.png")
 
     # Posición base del bloque (debajo del título)
-    y_bloque = 35
+    y_bloque = 25
 
     # Posición de la tabla (derecha)
-    tabla_x = 35
+    tabla_x = 100
     tabla_y = y_bloque
 
     # QR a la izquierda de la tabla (NO de la hoja)
