01d67ee7ed01c3dcd57c3644fec55458d1e86737
auxiliartecnologiastandard-lgt <auxiliartecnologiastandard@gmail.com>
2026-01-15 08:02:57 -0500

    Add files via upload

--- Cotizador_Coandes-copia/pages/Nev.py
+++ Cotizador_Coandes-copia/pages/Nev.py
@@ -150,8 +150,7 @@ def main():
         else:
             st.warning("Primero calcula el precio en el cotizador")
         
-    if st.session_state.get("precio_calculado"):
-            st.session_state["precio_calculado"] = True
+    if st.session_state["precio_calculado"] == True:
             if st.button("Crear contrato"):
                 # Esto funciona si el nombre de la página en Streamlit es 'Contrato_Standard'
                 st.switch_page("pages/Contrato_Standard.py")  # NO usar "pages/Contrato_Standard.py"Nev.py
