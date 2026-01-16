5a2e302c3c69351841764619dbe6f875836cf3ab
auxiliartecnologiastandard-lgt <auxiliartecnologiastandard@gmail.com>
2026-01-16 07:55:39 -0500

    Add files via upload

--- Cotizador_Coandes-copia/pages/Contrato_Standard.py
+++ Cotizador_Coandes-copia/pages/Contrato_Standard.py
@@ -195,7 +195,7 @@ elif datos["Origen"] == "Computador":
     qr.save("qr_temp.png")
 
     # Posición base del bloque (debajo del título)
-    y_bloque = 20
+    y_bloque = 15
 
     # Posición de la tabla (derecha)
     tabla_x = 150
@@ -204,7 +204,7 @@ elif datos["Origen"] == "Computador":
     # QR a la izquierda de la tabla (NO de la hoja)
     pdf.image(
         "qr_temp.png",
-        x=tabla_x - 15,
+        x=tabla_x - 13,
         y=tabla_y,
         w=17)
 
