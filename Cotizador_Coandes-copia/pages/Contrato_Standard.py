702172a885d2c56bb6d0d03d9f0d36d44c41e68f
auxiliartecnologiastandard-lgt <auxiliartecnologiastandard@gmail.com>
2026-01-23 15:50:54 -0500

    Add files via upload

--- Cotizador_Coandes-copia/pages/Contrato_Standard.py
+++ Cotizador_Coandes-copia/pages/Contrato_Standard.py
@@ -499,7 +499,7 @@ elif datos["Origen"] == "Computador":
         x=pdf.l_margin,
         y=22 + y_offset,
         w=pdf.w - pdf.l_margin - pdf.r_margin,
-        h=145)
+        h=130)
 
         # LOGO DE LA EMPRESA
         ruta_base = os.path.dirname(__file__)
