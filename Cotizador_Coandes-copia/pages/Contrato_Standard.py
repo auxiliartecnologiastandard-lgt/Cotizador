2ba629a6e6dd36b001845b82f8c0c828dfec29ff
auxiliartecnologiastandard-lgt <auxiliartecnologiastandard@gmail.com>
2026-01-26 09:25:16 -0500

    Add files via upload

--- Cotizador_Coandes-copia/pages/Contrato_Standard.py
+++ Cotizador_Coandes-copia/pages/Contrato_Standard.py
@@ -339,7 +339,7 @@ if datos["Origen"] == "Nevera":
             )
         
         pdf.set_draw_color(0, 0, 0)
-        pdf.rect(x=185, y=135 + y_offset, w=30, h=5)
+        pdf.rect(x=185, y=133 + y_offset, w=30, h=5)
         pdf.multi_cell(0, 1, f"Vence:  {fecha_vencimiento_str}", align="R")
 
         pdf.ln(7)
