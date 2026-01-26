0a62906223488d69ff01c1c677a99a046206729c
auxiliartecnologiastandard-lgt <auxiliartecnologiastandard@gmail.com>
2026-01-26 10:37:59 -0500

    Add files via upload

--- Cotizador_Coandes-copia/pages/Contrato_Standard.py
+++ Cotizador_Coandes-copia/pages/Contrato_Standard.py
@@ -354,15 +354,7 @@ if datos["Origen"] == "Nevera":
                 f"_____________________________\n{datos['Nombre']}\nC.C: {datos['Cedula']}",
                 align="L"   # L, C, R, J
                 )
-
-            pdf.set_xy(50, 126.4 + y_offset)   # Ajusta posición
-            pdf.cell(20, 18, "", border=1)
-
-            pdf.set_font("Arial","", size=5)
-            pdf.set_xy(50, 127.45 + y_offset)
-            pdf.cell(20, 17, "Huella del vendedor", align="C")
-
-
+            
             pdf.ln(3)
             pdf.set_xy(90, 1)
             pdf.set_font("Arial","B", size=8)
