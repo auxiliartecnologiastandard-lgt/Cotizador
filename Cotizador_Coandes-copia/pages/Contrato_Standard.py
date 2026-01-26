9e65bf33687a543f7a82f6128eb5ec6cd404ce67
auxiliartecnologiastandard-lgt <auxiliartecnologiastandard@gmail.com>
2026-01-26 10:18:34 -0500

    Add files via upload

--- Cotizador_Coandes-copia/pages/Contrato_Standard.py
+++ Cotizador_Coandes-copia/pages/Contrato_Standard.py
@@ -364,7 +364,7 @@ if datos["Origen"] == "Nevera":
 
 
             pdf.ln(3)
-            pdf.set_xy(90, 33)
+            pdf.set_xy(90, 1)
             pdf.set_font("Arial","B", size=8)
 
             # OTROS
