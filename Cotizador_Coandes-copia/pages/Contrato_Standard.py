3358d3b6bd8e6d09022c3a2b371ce25615133684
auxiliartecnologiastandard-lgt <auxiliartecnologiastandard@gmail.com>
2026-01-27 14:31:18 -0500

    Add files via upload

--- Cotizador_Coandes-copia/pages/Contrato_Standard.py
+++ Cotizador_Coandes-copia/pages/Contrato_Standard.py
@@ -673,12 +673,6 @@ elif datos["Origen"] == "Computador":
         pdf.set_draw_color(0, 0, 0)
         pdf.rect(x=185, y=128.5 + y_offset, w=30, h=5)
         pdf.multi_cell(0, 1, f"Vence:  {fecha_vencimiento_str}", align="R")
-        
-        
-        pdf.set_draw_color(0, 0, 0)
-        pdf.rect(x=185, y=131 + y_offset, w=30, h=4)
-        pdf.multi_cell(0, 1, f"Vence:  {fecha_vencimiento_str}", align="R")
-
 
         if y_offset >= 130:
 
