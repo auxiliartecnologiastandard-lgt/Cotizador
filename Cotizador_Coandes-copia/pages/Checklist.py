71b3b461d3a6b6d468d77c6f56e693a5c717ba04
auxiliartecnologiastandard-lgt <auxiliartecnologiastandard@gmail.com>
2026-01-30 15:22:25 -0500

    Add files via upload

--- Cotizador_Coandes-copia/pages/Checklist.py
+++ Cotizador_Coandes-copia/pages/Checklist.py
@@ -33,65 +33,36 @@ st.markdown(
 if st.button("⬅  Volver al Menú Principal"):
     st.switch_page("Lobby.py")
 
-base_datos = [
-    # Intel compatibles
-    "Intel Atom X7000 Series",
-    "Intel Celeron 3000 Series",
-    "Intel Celeron 4000 Series",
-    "Intel Celeron 5000 Series",
-    "Intel Celeron 6000 Series",
-    "Intel Celeron 7000 Series",
-    "Intel Core i3 (8th-14th Gen)",
-    "Intel Core i5 (8th-14th Gen)",
-    "Intel Core i7 (8th-14th Gen)",
-    "Intel Core i9 (8th-14th Gen)",
-    "Intel Core Ultra Series",
-    "Intel Pentium Gold",
-    "Intel Pentium Silver",
-    "Intel Xeon Series",
+st.markdown("### 1. Información del equipo") 
+Marca = st.text_input("Escribe la marca del equipo")
+Modelo = st.text_input("Escribe la marca del equipo")
+with st.expander("Como sacar el modelo del equipo"):
+    st.write('1. Presionar " Windows + R ", escribe cmd y preciona enter')
+    st.write('2. Escribe " wmic csproduct get name " y despues presiona Enter')
+    st.write('3. Aparecera la palabra " Name " y justo debajo el modelo del equipo')
+Serial = st.text_input("Escribe la marca del equipo")
+with st.expander("Como sacar el serial del equipo"):
+    st.write('1. Presionar " Windows + R ", escribe cmd y preciona enter')
+    st.write('2. Escribe " wmic bios get serialnumber " y despues presiona Enter')
+    st.write('3. Aparecera la palabra " SerialNumber " y justo debajo el serial del equipo')
+RAM = st.number_input("Escribe la RAM ( Solo el número )", min_value=1)
+Disco = st.number_input("Escribe la capacidad ( Solo el número )", min_value=1)
+Nombre = st.text_input("Escribe el nombre del equipo")
+with st.expander("Como sacar la RAM y capacidad del disco duro del equipo"):
+    st.write('1. Presionar la tecla windows ( El cuadrado dividdido en 4 )')
+    st.write('2. Escribe " Configuracion " y despues presiona Enter')
+    st.write('3. Busca el apartado que diga sistema')
+    st.write('4. En la parte lateral izquierda baja hasta encontrar una opción que diga " Acerca de " o " Más información " y dale click')
+    st.write('5. Hay encontraras la capacidad de la RAM, la capacidad del disco duro y el nombre del equipo')
+Bateria = st.number_input("Escribe el estado de la bateria", min_value=1)
+with st.expander("Como sacar el estado de la bateria"):
+    st.write('1. Presionar " Windows + X "y selecciona Windows powershell')
+    st.write('2. Copia ➡ " powercfg /batteryreport ", pegalo en powershell y despues presiona Enter')
+    st.write('3. Copia el link que te ofrecio powershell. Ejemplo: C:\\Users\\TuUsuario\\Battery-report.html.')
+    st.write('4. Pega ese link que copiaste en cualquier navegador ( Microsoft edge, Chrome, Firefox...)')
+    st.write('5. Busca un apartado que diga " Installed batteries " ( Esta al principio de la pagina )')
+    st.write('6. Busca en installed bateries secciones que digan " DESIGN CAPACITY " y " FULL CHARGE CAPACITY " ')
+    st.write('7. Divide el numero alfrente de FULL CHARGE CAPACITY por DESIGN CAPACITY y el resultado multiplicalo por 100')
+    st.write('8. Copia el numero en la casilla de texto aqui abajo ( ⚠ NO COPIES DECIMALES )')
 
-    # AMD compatibles
-    "AMD 3015e",
-    "AMD 3020e",
-    "AMD Athlon 3000 Series",
-    "AMD Athlon 7120 Series",
-    "AMD Athlon 7220 Series",
-    "AMD Ryzen 3 Series",
-    "AMD Ryzen 5 Series",
-    "AMD Ryzen 7 Series",
-    "AMD Ryzen Embedded R2000",
-    "AMD Ryzen Z1",
-    "AMD Ryzen Z1 Extreme",
-    "AMD EPYC Series"
-]
-
-st.divider()
-check_bateria = st.checkbox("Estado de la bateria")
-if not check_bateria:
-    with st.expander("📝 Como sacar el estado de la bateria"):
-        st.write('1. Presionar " Windows + X "y selecciona Windows powershell')
-        st.write('2. Copia ➡ " powercfg /batteryreport ", pegalo en powershell y despues presiona Enter')
-        st.write('3. Copia el link que te ofrecio powershell. Ejemplo: C:\\Users\\TuUsuario\\Battery-report.html.')
-        st.write('4. Pega ese link que copiaste en cualquier navegador ( Microsoft edge, Chrome, Firefox...)')
-        st.write('5. Busca un apartado que diga " Installed batteries " ( Esta al principio de la pagina )')
-        st.write('6. Busca en installed bateries secciones que digan " DESIGN CAPACITY " y " FULL CHARGE CAPACITY " ')
-        st.write('7. Divide el numero alfrente de FULL CHARGE CAPACITY por DESIGN CAPACITY y el resultado multiplicalo por 100')
-        st.write('8. Copia el numero en la casilla de texto aqui abajo ( ⚠ NO COPIES DECIMALES )')
-        observacion = st.number_input("Estado actual de la bateria", min_value=1, max_value=100 )
-        st.session_state["Estado_B"] = observacion
-
-col1, col2 = st.columns([3, 1])
-
-with col1:
-    st.divider()
-    st.session_state["Win_11"] = st.text_input("Procesador del equipo")
-with col2:
-    st.divider()
-    validar = st.button("Buscar procesador")
-    if validar:
-        if st.session_state["Win_11"] in base_datos:
-            st.success("✅ El procesador es compatible con windows 11")
-        else:
-            st.error("❌ El procesador NO es compatible con windows 11")
-st.divider()
-Descripcion = st.text_input("Agrega una descripcion del equipo")
+st.markdown("### 2. Pruebas post formateo") 
