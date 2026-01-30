9723a20b49d3bc354f68c1c14e25424bbbaaa226
auxiliartecnologiastandard-lgt <auxiliartecnologiastandard@gmail.com>
2026-01-30 07:52:59 -0500

    Add files via upload

--- Cotizador_Coandes-copia/pages/Checklist.py
+++ Cotizador_Coandes-copia/pages/Checklist.py
@@ -36,7 +36,7 @@ if st.button("⬅  Volver al Menú Principal"):
 with st.expander("📝 Como sacar el estado de la bateria"):
     st.write('1. Presionar "Windows + X"y selecciona Windows powershell')
     st.write('2. Copia ➡ " powercfg /batteryreport ", pegalo en powershell y despues presiona Enter')
-    st.write('3. Copia el link que te ofrecio powershell. Ejemplo: C:\Users\TuUsuario\Battery-report.html.')
+    st.write('3. Copia el link que te ofrecio powershell. Ejemplo: C:\\Users\\TuUsuario\\Battery-report.html.')
     st.write('4. Pega ese link que copiaste en cualquier navegador ( Microsoft edge, Chrome, Firefox...)')
     st.write('5. Busca un apartado que diga " Installed batteries " ( Esta al principio de la pagina )')
     st.write('6. Busca en installed bateries secciones que digan " DESIGN CAPACITY " y " FULL CHARGE CAPACITY " ')
