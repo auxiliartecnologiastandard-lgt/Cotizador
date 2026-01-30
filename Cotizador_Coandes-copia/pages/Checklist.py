552908c9b553306b5c3ebaec5dc57b1d4ca7256c
auxiliartecnologiastandard-lgt <auxiliartecnologiastandard@gmail.com>
2026-01-30 08:07:42 -0500

    Add files via upload

--- Cotizador_Coandes-copia/pages/Checklist.py
+++ Cotizador_Coandes-copia/pages/Checklist.py
@@ -33,15 +33,18 @@ st.markdown(
 if st.button("⬅  Volver al Menú Principal"):
     st.switch_page("Lobby.py")
 
-with st.expander("📝 Como sacar el estado de la bateria"):
-    st.write('1. Presionar "Windows + X"y selecciona Windows powershell')
-    st.write('2. Copia ➡ " powercfg /batteryreport ", pegalo en powershell y despues presiona Enter')
-    st.write('3. Copia el link que te ofrecio powershell. Ejemplo: C:\\Users\\TuUsuario\\Battery-report.html.')
-    st.write('4. Pega ese link que copiaste en cualquier navegador ( Microsoft edge, Chrome, Firefox...)')
-    st.write('5. Busca un apartado que diga " Installed batteries " ( Esta al principio de la pagina )')
-    st.write('6. Busca en installed bateries secciones que digan " DESIGN CAPACITY " y " FULL CHARGE CAPACITY " ')
-    st.write('7. Divide el numero alfrente de FULL CHARGE CAPACITY por DESIGN CAPACITY y el resultado multiplicalo por 100')
-    st.write('8. Copia el numero en la casilla de texto aqui abajo ( ⚠ NO COPIES DECIMALES )')
-    observacion = st.text_input(
-        "Observación",
-        placeholder="Ej: Pequeño rayón en la tapa, no afecta funcionamiento" )
+check_bateria = st.checkbox("Esrado de la bateria")
+
+if not check_bateria:
+    with st.expander("📝 Como sacar el estado de la bateria"):
+        st.write('1. Presionar " Windows + X "y selecciona Windows powershell')
+        st.write('2. Copia ➡ " powercfg /batteryreport ", pegalo en powershell y despues presiona Enter')
+        st.write('3. Copia el link que te ofrecio powershell. Ejemplo: C:\\Users\\TuUsuario\\Battery-report.html.')
+        st.write('4. Pega ese link que copiaste en cualquier navegador ( Microsoft edge, Chrome, Firefox...)')
+        st.write('5. Busca un apartado que diga " Installed batteries " ( Esta al principio de la pagina )')
+        st.write('6. Busca en installed bateries secciones que digan " DESIGN CAPACITY " y " FULL CHARGE CAPACITY " ')
+        st.write('7. Divide el numero alfrente de FULL CHARGE CAPACITY por DESIGN CAPACITY y el resultado multiplicalo por 100')
+        st.write('8. Copia el numero en la casilla de texto aqui abajo ( ⚠ NO COPIES DECIMALES )')
+        observacion = st.text_input(
+            "Observación",
+            placeholder="Estado de la bateria" )
